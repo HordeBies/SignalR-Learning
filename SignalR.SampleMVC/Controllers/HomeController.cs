1a347eac1f280ab7c4d69ecb47ca6e45c14f9c6c
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-07-14 17:54:08 +0300

    Add project files.

---
+++ SignalR.SampleMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalR.SampleMVC.Models;
+using System.Diagnostics;
+
+namespace SignalR.SampleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
